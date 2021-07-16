2433e00eae6db158a23f6822cb5cd533c755bd2f
Anthony <anthonybeavermusic@gmail.com>
2021-07-15 21:53:15 -0700

    bug fixes and save txt file persistance

--- get_name.py
+++ get_name.py
@@ -37,16 +37,18 @@ def student_selector():
     # Updated after the random choice
     completed_students = []
 
-    # Try getting the list of already called on students. If there is not a chosen_students.txt a exception will print.
+    print('Loading the previously saved students')
+
+    # Getting the list of already called on students from saved_students.txt. If there is not a saved_students.txt a exception will print.
     try:
-        with open('chosen_students.txt') as chosen_already:
+        with open('saved_students.txt') as chosen_already:
             students_read = chosen_already.read()
             split_students = students_read.split()
             # print(split_students)
             for each_student in split_students:
                 completed_students.append(each_student)
     except:
-        print('No chosen_students.txt found')
+        print('No saved_students.txt found?')
 
     students_left = []
     half = []
@@ -57,28 +59,32 @@ def student_selector():
         if student not in completed_students:
             students_left.append(student)
     
-    # If there is only one student left we default to that student and give the input options.
-    if len(students_left) == 1:
-        print(f'\nAnd the final winner is: {students_left[0]} \n')
-        print('All students have already been chosen once \n')
-        print('Clearing the already selected list')
-        clear_chosen()
-        choice = input('Would you like to: \nChoose a new student (y) \nClear the current list and run this again (c)\nSave this list and exit? (s) \nClear the current list and Exit (e) \n')
-        if choice == 'y':
-            student_selector()
+    # If all the students have already been called on and stored in the saved_students.txt this will show up:
+    if len(students_left) == 0:
+        print('All students have already gone, check saved_students.txt.\n')
+        choice = input('Would you like to: \nClear the saved list and run this again (c)\nExit (e) \n')
         if choice == 'c':
             clear_chosen()
             student_selector()
-        if choice == 's':
-            save_chosen(completed_students)
-            print('List has been saved to saved_students.txt, thanks for using the app!')
-            return
         else:
+            print('Exiting, thanks for using the app!')
+            return
+
+    # If there is only one student left we default to that student and give the input options.
+    if len(students_left) == 1:
+        print(f'\nAnd the final winner is: {students_left[0]} \n')
+        completed_students.append(students_left[0])
+        save_chosen(completed_students)
+        print('All students have already been chosen once. \n')
+        choice = input('Would you like to: \nClear the saved list and run this again (c)\nExit (e) \n')
+        if choice == 'c':
             clear_chosen()
-            print('List cleared, thanks for using the app!')
+            student_selector()
+        else:
+            print('Exiting, thanks for using the app!')
             return
     else:
-        for i in range(0, len(students) // 2 ):
+        for i in range(0, len(students_left) // 2 ):
             student_entered = False
             while not student_entered:
                 random_student = random.choice(students_left)
@@ -95,18 +101,20 @@ def student_selector():
 
         print(f'\n\n***** Cutting Down the Choices: {half}')
         # time.sleep(3.5)
-
-        for i in range(0, len(half) // 2 ):
-            student_entered = False
-            while  not student_entered:
-                random_student = random.choice(half)
-                if do_print:  print(random_student)
-                if random_student in quarter:
-                    if do_print:  print('Student Already there.  Getting another one')
-                    continue
-                else:
-                    quarter.append(random_student)
-                    student_entered = True
+        if len(half) == 1:
+            quarter.append(half[0])
+        else:
+            for i in range(0, len(half) // 2 ):
+                student_entered = False
+                while  not student_entered:
+                    random_student = random.choice(half)
+                    if do_print:  print(random_student)
+                    if random_student in quarter:
+                        if do_print:  print('Student Already there.  Getting another one')
+                        continue
+                    else:
+                        quarter.append(random_student)
+                        student_entered = True
 
         print(f'\n\n***** Down a few more: {quarter}')
         print('\n\n*****Pausing for Dramatic Effect!!!*****')
@@ -115,21 +123,17 @@ def student_selector():
         print(f'\nAnd the winner is: {chosen} \n')
         completed_students.append(chosen)
         print(f'Already chosen students = {completed_students}')
-        update_chosen_file(completed_students)
+        # update_chosen_file(completed_students)
+        save_chosen(completed_students)
+        print('saved_students.txt has been updated.')
 
-        save_or_choose = input('Would you like to: \nChoose a new student (y) \nClear the current list and run this again (c)\nSave this list and exit? (s) \nClear the current list and Exit (e) \n')
-        if save_or_choose == 's':
-            save_chosen(completed_students)
-            print('List has been saved to saved_students.txt, thanks for using the app!')
-            return
-        elif save_or_choose == 'y':
+        save_or_choose = input('Would you like to: \nChoose a new student (y) \nClear the saved list and run this again (c)\nExit (e)\n')
+        if save_or_choose == 'y':
             student_selector()
         elif save_or_choose == 'c':
-            clear_chosen()
             student_selector()
         else:
-            clear_chosen()
-            print('List cleared, thanks for using the app!')
+            print('Exiting, thanks for using the app!')
             return
 
 
@@ -142,24 +146,24 @@ def save_chosen(save_students):
             students_to_save += f'{each} '
         to_save.write(students_to_save)
 
-def update_chosen_file(prev_chosen_students):
+# def update_chosen_file(prev_chosen_students):
 
-    ## Logic for writing each student's name to a file.
-    # with open('student_list.txt', 'w') as studend_list:   
-    #     all_students = ''
+#     ## Logic for writing each student's name to a file.
+#     # with open('student_list.txt', 'w') as studend_list:   
+#     #     all_students = ''
 
-    #     for each_student in students:
-    #         all_students += f'{each_student} '
-    #     studend_list.write(all_students)
+#     #     for each_student in students:
+#     #         all_students += f'{each_student} '
+#     #     studend_list.write(all_students)
 
-    with open('chosen_students.txt', 'w') as chosen_already:
-        already_called = ''
-        for each_student in prev_chosen_students:
-            already_called += f'{each_student} '
-        chosen_already.write(already_called)
+#     with open('chosen_students.txt', 'w') as chosen_already:
+#         already_called = ''
+#         for each_student in prev_chosen_students:
+#             already_called += f'{each_student} '
+#         chosen_already.write(already_called)
 
 def clear_chosen():
-    with open('chosen_students.txt', 'w') as chosen_already:
+    with open('saved_students.txt', 'w') as chosen_already:
         already_called = ''
         chosen_already.write(already_called)
 
