6f157ab9c734b8777c98bd4462c36c69d05a51ba
Anthony <anthonybeavermusic@gmail.com>
2021-07-26 21:39:05 -0700

    group randomizer push

--- random_team.py
+++ random_team.py
@@ -1,5 +1,22 @@
 import random
+import re
 
+students = []
+prev_groups = []
+
+# Getting all the students from the student_list.txt
+with open('student_list.txt') as student_list:
+    read_students = student_list.read()
+    split_students = read_students.split()
+    for each in split_students:
+        students.append(each)
+
+with open("prev_group.txt") as prev_group:
+    read_group = prev_group.read()
+    regex = re.findall(r"\[[^\]]*\]", read_group)
+    
+    for each in regex:
+        prev_groups.append(each)
 
 def stringify(L: list[list[str]]) -> str:
     """Format the given list of lists of string values into the string that has those items printed as a group of values divided with ' - ' with each group starting from the new line
@@ -35,39 +52,40 @@ def get_random_pairs(students: list, students_per_group: int = 4) -> str:
 
     while group <= number_of_groups:
         if group != number_of_groups:
-            output.append(
-                students[group * students_per_group:(group * students_per_group) + students_per_group])
+            to_group = students[group * students_per_group:(group * students_per_group) + students_per_group]
+            if str(to_group) not in prev_groups:
+                output.append(to_group)
         else:
             output.append(students[group * students_per_group:])
         group += 1
 
+    save_group(output)
     return stringify(output)
+
+def save_group(save_students):
+    with open('prev_group.txt', 'w') as to_save:
+        groups_to_save = ''
+        for each in save_students:
+            groups_to_save += f'{each}'
+        to_save.write(groups_to_save)
 
 
 if __name__ == "__main__":
-    STUDENTS = [
-        # Absent students can be commented out below
-        'Karlo',
-        'Alexander',
-        'Amber',
-        'Anthony',
-        'Ashley',
-        'Audrena',
-        'Ben',
-        'Bhagirath',
-        'Brandon',
-        'Grace',
-        'Jae',
-        'Kim',
-        'Logan',
-        'Mason',
-        'Matthew',
-        'Nebiyu',
-        'Nick',
-        'Samuel',
-        'Sean',
-        'Sian'
-        ]
-    group_number = int(input('How many students per group? '))
+    group_number = input('How many students per group? \nOr press enter for 2 \n')
+    # if group_number == "":
+    #     group_number = 2
+    # else:
+    while type(group_number) != int:
+        if group_number == "":
+            group_number = 2
+        try:
+            group_number = int(group_number)
+            # group = get_random_pairs(students, group_number)
+            # print(group)
+        except ValueError:
+            print('Not an integer')
+            group_number = input('How many students per group? \nOr press enter for 2 \n')
 
-    print(get_random_pairs(STUDENTS, group_number))
+    group = get_random_pairs(students, group_number)
+    print(group)
+
